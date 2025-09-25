22199f103b25b831ae42178fea8b1ebb5a497ae4
德华 刘 <gavinprior5@gmail.com>
2025-09-25 20:44:03 +0000

    adjust body styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar6 } from "@/components/layout/Navigation";
+import { Footer6 } from "@/components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,10 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background dark`}
       >
         <Navbar6 />
         {children}
+        <Footer6 />
       </body>
     </html>
   );
